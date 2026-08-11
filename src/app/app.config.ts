8917039c163f6d8984886d7ec65b77cd17b80801
Andreas Blomqvist <ablomq@users.noreply.github.com>
2026-08-11 08:45:23 +0200

    Update 2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, RouteReuseStrategy } from '@angular/router';
 import { routes } from './app.routes';
 import { CustomRouteReuseStrategy } from './custom-route-reuse-strategy';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     { provide: RouteReuseStrategy, useClass: CustomRouteReuseStrategy },
   ],
